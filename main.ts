24554d947dd80f65743c334b134a3e2e6c454d63
LuisLi18 <81203064+LuisLi18@users.noreply.github.com>
2021-11-27 12:34:12 -0500

    Delete main.ts

--- main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  console.log(__dirname);
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
-}
-bootstrap();
